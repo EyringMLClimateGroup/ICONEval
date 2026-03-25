e627bbbf5263bfb5270e48423da4ef20a7326d34
Manuel Schlund <manuel.schlund@dlr.de>
2026-03-25 13:27:36 +0100

    Use shorter anomaly reference period

--- tests/generate_expected_output.py
+++ tests/generate_expected_output.py
@@ -10,7 +10,7 @@ TESTS_ROOT = Path(__file__).parent
 
 def main() -> None:
     """Generate expected output."""
-    temp_dir = tempfile.mkdtemp(
+    tmp_dir = tempfile.mkdtemp(
         prefix="expected_output_tmp_",
         dir=TESTS_ROOT,
     )
@@ -20,12 +20,16 @@ def main() -> None:
         "-m",
         "uses_expected_output",
         "--generate_expected_output",
-        str(temp_dir),
+        str(tmp_dir),
         str(TESTS_ROOT),
     ]
     pytest.main(pytest_args)
 
-    print("Saved expected output to", temp_dir)  # noqa: T201
+    original_output = TESTS_ROOT / "expected_output"
+    print(f"Saved expected output to {tmp_dir}")  # noqa: T201
+    print(  # noqa: T201
+        f"Run `rm -rf {original_output} && mv {tmp_dir} {original_output}` to save it",
+    )
 
 
 if __name__ == "__main__":
