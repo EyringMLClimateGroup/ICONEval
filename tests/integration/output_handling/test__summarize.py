586718f2b95521d4caa893fb64af0d7d89a50af5
Manuel Schlund <manuel.schlund@dlr.de>
2026-07-27 10:46:09 +0200

    Simplify summarize test

--- tests/integration/output_handling/test__summarize.py
+++ tests/integration/output_handling/test__summarize.py
@@ -1,26 +1,16 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 import pytest
 
 from iconeval.output_handling._summarize import summarize
 
 if TYPE_CHECKING:
-    from pathlib import Path
 
     from pytest_datadir.plugin import LazyDataDir
 
     from tests.integration import OutputDirRegression
-
-
-def run_summarize_regression_test(
-    esmvaltool_output: Path,
-    output_dir_regression: OutputDirRegression,
-    **kwargs: Any,
-) -> None:
-    summarize(esmvaltool_output, **kwargs)
-    output_dir_regression.check(esmvaltool_output)
 
 
 @pytest.mark.parametrize(
@@ -35,21 +25,18 @@ def test_summarize(
     output_dir_regression: OutputDirRegression,
     lazy_shared_datadir: LazyDataDir,
 ) -> None:
-    run_summarize_regression_test(
-        lazy_shared_datadir / "recipes_zonal-means",
-        output_dir_regression,
-        description=description,
-    )
+    esmvaltool_output = lazy_shared_datadir / "recipes_zonal-means"
+    summarize(esmvaltool_output, description=description)
+    output_dir_regression.check(esmvaltool_output)
 
 
 def test_summarize_empty_logs(
     output_dir_regression: OutputDirRegression,
     lazy_shared_datadir: LazyDataDir,
 ) -> None:
-    run_summarize_regression_test(
-        lazy_shared_datadir / "recipes_maps",
-        output_dir_regression,
-    )
+    esmvaltool_output = lazy_shared_datadir / "recipes_maps"
+    summarize(esmvaltool_output)
+    output_dir_regression.check(esmvaltool_output)
 
 
 def test_summarize_no_debug_log(
@@ -59,7 +46,8 @@ def test_summarize_no_debug_log(
     esmvaltool_output = lazy_shared_datadir / "recipes_maps"
     debug_log = esmvaltool_output / "recipe_basics_maps" / "run" / "main_log_debug.txt"
     debug_log.unlink()
-    run_summarize_regression_test(esmvaltool_output, output_dir_regression)
+    summarize(esmvaltool_output)
+    output_dir_regression.check(esmvaltool_output)
 
 
 def test_summarize_debug_log_single_line(
@@ -69,4 +57,5 @@ def test_summarize_debug_log_single_line(
     esmvaltool_output = lazy_shared_datadir / "recipes_maps"
     debug_log = esmvaltool_output / "recipe_basics_maps" / "run" / "main_log_debug.txt"
     debug_log.write_text("this is a single line that cannot be used to infer runtime")
-    run_summarize_regression_test(esmvaltool_output, output_dir_regression)
+    summarize(esmvaltool_output)
+    output_dir_regression.check(esmvaltool_output)
