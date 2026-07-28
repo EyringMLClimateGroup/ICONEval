854e7370f145946e26a71739d1eacf26b2ec57a0
Manuel Schlund <32543114+schlunma@users.noreply.github.com>
2026-07-28 10:27:45 +0200

    Complete repository transfer (#56)
    * Regenerate tags.md
    
    * Temporarily deleted coverage badge
    
    * Use correct link for tags
    
    * Updated repo links
    
    * Remove Veronika from list of contacts
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix Zenodo badge
    
    * Automatically create pull request for coverage badge updates
    
    * Ignore pytest.xml files
    
    * Manually checking for diffs is not necessary
    
    * Do not run update coverage action in this branch
    
    * Update coverage badge (#58)
    
    Co-authored-by: schlunma <32543114+schlunma@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- doc/generate_tag_table.py
+++ doc/generate_tag_table.py
@@ -36,8 +36,8 @@ TAGS_FILE: Path = Path(__file__).parent / "tags.md"
 def _get_link(recipe_template: RecipeTemplate) -> str:
     """Get link to recipe template."""
     return (
-        f"[{recipe_template.name}](https://github.com/EyringMLClimateGroup/ICONEval/"
-        f"blob/main/iconeval/recipe_templates/{recipe_template.path.name})"
+        f"[{recipe_template.name}](https://github.com/ESMValGroup/ICONEval/"
+        f"blob/main/src/iconeval/recipe_templates/{recipe_template.path.name})"
     )
 
 
